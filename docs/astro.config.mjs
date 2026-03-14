7662edbfe807f8af9ffb0ecc3df83e155cc165d0
Arya <arya@k-id.com>
2026-03-14 23:42:44 +0800

    docs: add site and base to astro config for github pages

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config'
 import starlight from '@astrojs/starlight'
 
 export default defineConfig({
+  site: 'https://aryabyte21.github.io',
+  base: '/koto',
   integrations: [
     starlight({
       title: 'koto',
